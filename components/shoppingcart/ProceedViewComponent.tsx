549d3cb2bd1a1a6ab0e4f3ef92160a56b4a0e4f6
Zekaria Abdirahman Mohamed <zekariaam@student.uia.no>
2023-04-14 21:27:59 +0200

    fix: fix eslint errors

--- components/shoppingcart/ProceedViewComponent.tsx
+++ components/shoppingcart/ProceedViewComponent.tsx
@@ -3,8 +3,6 @@ import React from "react";
 import cartstyle from "../../styles/cartstyles";
 import TotalOrdersComponent from "./TotalOrdersComponent";
 import CheckoutButtonComponent from "./CheckoutButtonComponent";
-
-import { UserProvider } from "../Auth/userContext";
 export default function ProceedViewComponent() {
   return (
     <View style={cartstyle.totalorder}>
