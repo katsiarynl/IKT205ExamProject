14f6d9e97908f93ed6d17ffdfa24219428321060
Zekaria Abdirahman Mohamed <zekariaam@student.uia.no>
2023-05-08 17:28:23 +0200

    fex: move loading form restrauntItem

--- components/restaurants/RestrauntItemComponent.tsx
+++ components/restaurants/RestrauntItemComponent.tsx
@@ -1,24 +1,22 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import { View } from "react-native";
 import { Card, Text } from "react-native-paper";
 import { SvgXml } from "react-native-svg";
 import star from "../../assets/star";
-import { ActivityIndicator, Colors } from "react-native-paper";
 
 import { restaurantStyle } from "../../styles/restaurantsStyle";
 import { RestrauntItemComponentType } from "../../types/restrauntTypes";
 import GETPath from "../../utilities/GETPath";
 import fetchImage from "../../utilities/FetchImage";
-import { UserContext } from "../Auth/userContext";
 
 export default function RestrauntItemComponent(
   props: RestrauntItemComponentType
 ) {
   const ratingArray = Array.from({ length: Math.floor(props["rating"]) });
   const [avatare, setAvatar] = useState("");
-  const [isLoading, setisLoading] = useState<boolean>(false);
 
-  return !isLoading ? (
+  fetchImage(GETPath, setAvatar, props.image);
+  return (
     <View>
       <Card elevation={5} style={restaurantStyle.cardStyle}>
         <Card.Cover
@@ -36,7 +34,5 @@ export default function RestrauntItemComponent(
         <Text style={restaurantStyle.address}>{props.address}</Text>
       </Card>
     </View>
-  ) : (
-    <ActivityIndicator animating={true} color={Colors.blue300} />
   );
 }
