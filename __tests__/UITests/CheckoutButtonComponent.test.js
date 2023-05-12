8994987bbf1660e2cae3271844513b4591e6ce03
Katsiaryna Labkouskaya <katsiarynl@uia.no>
2023-05-12 12:51:58 +0200

    refactor: restructure tests dir

--- __tests__/UITests/CheckoutButtonComponent.test.js
+++ __tests__/UITests/CheckoutButtonComponent.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { create } from "react-test-renderer";
-import CheckoutButtonComponent from "../components/shoppingcart/ChangeQuantityButton";
+import CheckoutButtonComponent from "../../components/shoppingcart/CheckoutButtonComponent";
 const tree = create(<CheckoutButtonComponent />);
 
 test("snapshot", () => {
--- utilities/DecreaseItemQuantity.ts
+++ utilities/DecreaseItemQuantity.ts
@@ -1,4 +1,5 @@
-const DecreaseQuantity = (dispatch, cartitem) => {
+import { ActionsType } from "../components/Auth/userContext";
+const DecreaseQuantity = (dispatch: React.Dispatch<ActionsType>, cartitem) => {
   dispatch({
     type: "REMOVE_ORDER",
     payload: cartitem,
